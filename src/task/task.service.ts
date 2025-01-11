562c2af1d893438452cb10e0ea00b7a9549c0894
omkar-eng <thakuromkar600@gmail.com>
2025-01-11 12:44:26 +0530

    first commit

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -2,68 +2,51 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TaskService {
-    private readonly dataStored = [{taskNumber: 1,taskDuration: 3,dependencies: [], completed: false},
-    {taskNumber: 2,taskDuration: 5,dependencies: [1], completed: false},
-    {taskNumber: 3,taskDuration: 2,dependencies: [], completed: false},
-    {taskNumber: 4,taskDuration: 4,dependencies: [2, 3], completed: false}];
-  getHello(): string {
-    return 'Hello World!';
-  }
-
-  async createTask(taskNumber: number, taskDuration: number, dependencies: number[]): Promise<string> {
-    const obj = {
-        taskNumber,
-        taskDuration,
-        dependencies, 
-        completed: false
+    private readonly maxConcurrent = 2;
+    private dataStored = [{ taskNumber: 1, taskDuration: 3, dependencies: [], completed: false },
+    { taskNumber: 2, taskDuration: 5, dependencies: [1], completed: false },
+    { taskNumber: 3, taskDuration: 2, dependencies: [], completed: false },
+    { taskNumber: 4, taskDuration: 4, dependencies: [2, 3], completed: false }];
+    getHello(): string {
+        return 'Hello World!';
     }
-    this.dataStored.push(obj);
-    return `Task is created`;
-  }
 
-  async runScheduler() {
-    const data = this.dataStored;
-    data.sort((a: any, b: any) => a.taskDuration - b.taskDuration);
-    const promises = [];
-    for(let index = 0; index < this.dataStored.length; index +=1) {
-        let {
+    async createTask(taskNumber: number, taskDuration: number, dependencies: number[]): Promise<string> {
+        const obj = {
             taskNumber,
             taskDuration,
             dependencies,
-            completed
-        } = this.dataStored[index];
-
-        let promise;
-        console.log(`Task ${taskNumber} Started`)
-        if (dependencies.length) { 
-            let totalTime = 0;
-            for (let i =0; i < dependencies.length; i++) {
-                const dependency = dependencies[i];
-                this.dataStored.map((ele) => {
-                    if(ele.taskNumber === dependency) {
-                        totalTime = totalTime + ele.taskDuration;
-                    }
-                })
-            }
-            promise = new Promise(function(resolve, reject) {
-                setTimeout(function () {
-                   completed =  true;
-                   resolve(`Task ${taskNumber} Completed`)
-                }, taskDuration + totalTime);
-            });
-        } else {
-            promise = new Promise(function(resolve, reject) {
-                setTimeout(function () {
-                   completed =  true;
-                   resolve(`Task ${taskNumber} Completed`)
-                }, taskDuration);
-            });
+            completed: false
         }
-        promises.push(promise);
+        this.dataStored.push(obj);
+        return `Task is created`;
     }
-    const result =  await Promise.all(promises);
-    result.map((ele) => {
-        console.log(ele);
-    })
-  }
+
+    async runTask(dataStored) {
+        console.log(`Task ${dataStored.taskNumber} started`);
+        await new Promise(resolve => setTimeout(resolve, dataStored.taskDuration * 10));
+        console.log(`Task ${dataStored.taskNumber} completed`);
+        dataStored.completed = true;
+    }
+
+    async runScheduler() {
+        const runningTasks: Promise<void>[] = [];
+        while (this.dataStored.length > 0) {
+            for (const task of this.dataStored) {
+                if (!task.completed && task.dependencies.every(dep => this.dataStored[dep - 1].completed)) {
+                    if (runningTasks.length < this.maxConcurrent) {
+                        runningTasks.push(this.runTask(task));
+                    }
+                }
+            }
+            if (runningTasks.length > 0) {
+                await Promise.race(runningTasks);
+                this.dataStored = this.dataStored.filter(task => !task.completed);
+            } else {
+                break;
+            }
+        }
+
+        await Promise.all(runningTasks);
+    }
 }
